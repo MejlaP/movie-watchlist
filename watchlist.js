41b46505b7aba58db1e0b12b9b5b0aa9c50c0269
mejlap <miloshp8@gmail.com>
2022-08-25 09:25:03 +0200

    watchlist finished

--- watchlist.js
+++ watchlist.js
@@ -1,14 +1,16 @@
 // get items from local storage for following render
 let moviesFromLocalStorage = JSON.parse(localStorage.getItem('session'))
+console.log(moviesFromLocalStorage)
 
 // grab element
 const foundedMovies = document.getElementById('found-movies')
 
 // function for rendering movies
 function renderMoviesFromLocalStorage(moviesArray) {
-    let htmlMovies = ""
-    for (const movie of moviesArray) {
-        htmlMovies += `<div class="movie-container">
+    if (moviesArray !== null && moviesArray.length) {
+        let htmlMovies = ""
+        for (const movie of moviesArray) {
+            htmlMovies += `<div class="movie-container movie-watchlist">
                             <img src="${movie.Poster}" alt="found-movie">
                             <div class="movie-card">
                                 <div class="movie-title">
@@ -27,10 +29,20 @@ function renderMoviesFromLocalStorage(moviesArray) {
                                 <p class="movie-description">${movie.Plot}</p>
                             </div>
                         </div>`
-    }
+        }
 
-    // element htmlMovies as innerHTML
-    foundedMovies.innerHTML = htmlMovies
+        // element htmlMovies as innerHTML
+        foundedMovies.innerHTML = htmlMovies
+    }
+    else {
+        foundedMovies.innerHTML = `<div class="empty-watchlist">
+                                        <h6>Your watchlist is looking a little empty...</h6>
+                                        <div class="empty-watchlist-text-icon">
+                                        <img src="./img/addIcon.png"/>
+                                        <p>Let’s add some movies!</p>
+                                        </div>
+                                   </div>`
+    }
 }
 
 // render movies from local storage
@@ -38,10 +50,10 @@ renderMoviesFromLocalStorage(moviesFromLocalStorage)
 
 // function for deleting movie
 const deleteMovie = (IdOfMovie) => {
-    
+
     // get index of object with id of IdOfMovie
     const removeMovieById = moviesFromLocalStorage.findIndex(movie => movie.imdbID === IdOfMovie);
-    
+
     // remove object
     moviesFromLocalStorage.splice(removeMovieById, 1)
 
